a6315acabb7868f1e5ecbc9a0f73fe8da01e4b56
Eddersouza <eddyesouza@gmail.com>
2019-10-09 22:10:34 -0300

    [FEAT(CriarNovoFornecedor)]  - Entity and entity map to Client Provider

---
+++ Back/App/MFI.Data.EF/EntityMap/ClientProviderMap.cs
@@ -0,0 +1,22 @@
+﻿using MFI.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace MFI.Data.EF.EntityMap
+{
+    public class ClientProviderMap
+        : EntityTypeConfiguration<ClientProvider>
+    {
+        public ClientProviderMap()
+        {
+            ToTable("Clients");
+
+            Property(c => c.Description)
+                .HasMaxLength(ClientProvider.DescriptionMaxLength)
+                .IsRequired();
+
+            Property(c => c.CompanyName)
+                .HasMaxLength(ClientProvider.CompanyNameMaxLength)
+                .IsOptional();
+        }
+    }
+}
---
+++ Back/App/MFI.Domain/Entities/ClientProvider.cs
@@ -0,0 +1,31 @@
+﻿namespace MFI.Domain.Entities
+{
+    public class ClientProvider : Client
+    {
+        public const int CompanyNameMaxLength = 80;
+        public const int CompanyNameMinLength = 10;
+        public const int DescriptionMaxLength = 500;
+        public const int DescriptionMinLength = 10;
+
+
+        public ClientProvider()
+        {
+        }
+
+        public ClientProvider(
+           string email,
+           string name,
+           string companyName,
+           string description,
+           User user) : base(email, name, user)
+        {
+            this.Type = Enums.ClientType.Provider;
+            this.CompanyName = companyName;
+            this.Description = description;
+        }
+
+        public string Description { get; set; }
+
+        public string CompanyName { get; set; }
+    }
+}
