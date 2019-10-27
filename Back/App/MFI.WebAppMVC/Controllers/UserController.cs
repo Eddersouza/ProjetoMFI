a58c89596b9e064cf09b356e4183b518748b4180
Eddersouza <eddyesouza@gmail.com>
2019-10-27 15:02:06 -0300

    [FEAT(CriarNovoCliente)] - Criação modal Criação usuario/Entrar no sistema

---
+++ Back/App/MFI.WebAppMVC/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace MFI.WebAppMVC.Controllers
+{
+    [RoutePrefix("Usuario")]
+    public class UserController : Controller
+    {
+        [Route("Tipo")]
+        public ActionResult ChooseType()
+        {
+            return View();
+        }
+    }
+}
