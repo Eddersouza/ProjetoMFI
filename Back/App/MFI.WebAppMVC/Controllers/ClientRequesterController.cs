2c90c5ed3c1d7a4b2ebb17cfd37d52051c717519
Eddersouza <eddyesouza@gmail.com>
2019-11-01 20:31:48 -0300

    [REFACTOR(CriarNovoCliente)] - Front criação

---
+++ Back/App/MFI.WebAppMVC/Controllers/ClientRequesterController.cs
@@ -0,0 +1,22 @@
+﻿using MFI.Application.ViewModels.Clients.Requesters;
+using System.Web.Mvc;
+
+namespace MFI.WebAppMVC.Controllers
+{
+    [RoutePrefix("Cliente")]
+    public class ClientRequesterController : Controller
+    {
+        [Route("Novo")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("")]
+        public JsonResult Create(CreateClientRequester create)
+        {
+            return Json(null);
+        }
+    }
+}
