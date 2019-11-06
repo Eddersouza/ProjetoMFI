76336e6ad1c313800efe30d5f4663f2552a9919a
Eddersouza <eddyesouza@gmail.com>
2019-11-05 22:04:13 -0300

    [FEAT(CriarNovoFornecedor)] - Tela novo fornecedor

---
+++ Back/App/MFI.WebAppMVC/Controllers/ClientProviderController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace MFI.WebAppMVC.Controllers
+{
+    [RoutePrefix("Fornecedor")]
+    public class ClientProviderController : Controller
+    {
+        [Route("Novo")]
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
