ccb2eb9970db1d18c90a028de241b60e5585fbf4
Eddersouza <eddyesouza@gmail.com>
2019-11-07 19:48:58 -0300

    [FEAT(CriarNovoFornecedor)] - Tela e Jquery

--- Back/App/MFI.WebAppMVC/Controllers/ClientProviderController.cs
+++ Back/App/MFI.WebAppMVC/Controllers/ClientProviderController.cs
@@ -1,10 +1,52 @@
-﻿using System.Web.Mvc;
+﻿using MFI.Application.Base;
+using MFI.Application.Interfaces;
+using MFI.Application.ViewModels.Clients.Providers;
+using System.Web.Mvc;
 
 namespace MFI.WebAppMVC.Controllers
 {
     [RoutePrefix("Fornecedor")]
     public class ClientProviderController : Controller
     {
+        private readonly ClientProviderAppContract _clientProviderApp;
+
+        public ClientProviderController(
+            ClientProviderAppContract clientProviderApp)
+        {
+            this._clientProviderApp = clientProviderApp;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public JsonResult Create(
+           CreateClientProvider client)
+        {
+            MFIResultContract result = null;
+            try
+            {
+                result = _clientProviderApp.Create(client);
+
+                if (result.HasSuccess)
+                    return Json(result);
+
+                Response.StatusCode = 400;
+
+                return Json(result);
+            }
+            catch
+            {
+                result = new MFIResult();
+
+                result.AddWarning("Ocorreu um erro ao executar a ação.");
+                result.AddWarning("Tente novamente ou entre em contato com o administrador.");
+
+                Response.StatusCode = 500;
+
+                return Json(result);
+            }
+        }
+
+        [HttpGet]
         [Route("Novo")]
         public ActionResult New()
         {
--- Back/App/MFI.WebAppMVC/Scripts/pages/requester/new-provider.js
+++ Back/App/MFI.WebAppMVC/Scripts/pages/requester/new-provider.js
@@ -1,1 +1,20 @@
-﻿
+﻿function btnCreateProvider_OnClick() {
+    let formElements = $('.new-requester-form');
+
+    let newObjectForm = {};
+
+    for (var i = 0; i < formElements.length; i++) {
+        newObjectForm[formElements[i].name] = formElements[i].value;
+    }
+}
+
+
+function onDocumentNewProviderReady() {
+    setNewRequestActions();
+}
+
+function setNewRequestActions() {
+    $('body').delegate('#btnCreateProvider', 'click', btnCreateProvider_OnClick);
+}
+
+$(document).ready(onDocumentNewProviderReady);
