d7ee00c4b054df280996cf941ca248b7202d4937
eddersouza <eddye_souza@yahoo.com.br>
2019-11-11 22:33:54 -0300

    [FEAT(EntrarComoFornecedor)] - login

--- Back/App/MFI.WebAppMVC/Controllers/BaseController.cs
+++ Back/App/MFI.WebAppMVC/Controllers/BaseController.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using MFI.Domain.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Security.Claims;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Web.Security;
 
 namespace MFI.WebAppMVC.Controllers
 {
     public class BaseController : Controller
     {
+        public SystemUser SystemUser
+        {
+            get
+            {
+                SystemUser user = null;
+
+                if (HttpContext.Request.IsAuthenticated)
+                {
+                    FormsAuthenticationTicket authTicket = null;
+                    var authCookie = HttpContext.Request.Cookies[FormsAuthentication.FormsCookieName];
+                    if (authCookie != null)
+                    {
+                        authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                        if (authTicket != null && !authTicket.Expired)
+                        {
+                            user = new JavaScriptSerializer().Deserialize<SystemUser>(authTicket.UserData);
+                        }
+                    }
+                }
+
+                return user;
+            }
+        }
+
         public bool IsAuthenticated
         {
             get { return HttpContext.User.Identity.IsAuthenticated; }
@@ -12,6 +42,7 @@ namespace MFI.WebAppMVC.Controllers
 
         protected override void EndExecute(IAsyncResult asyncResult)
         {
+            ViewBag.Name = SystemUser?.UserName;
             ViewBag.IsAuthenticated = IsAuthenticated;
             base.EndExecute(asyncResult);
         }
